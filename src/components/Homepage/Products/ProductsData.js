09fd9dc262b3ac3b0fae4fb5301752e0497b6178
Phil <pjguitar15@gmail.com>
2021-03-31 13:18:41 +0800

    separated products data

---
+++ src/components/Homepage/Products/ProductsData.js
@@ -0,0 +1,58 @@
+export const mockData = [
+  {
+    id: 1,
+    name: 'Shoes',
+    description: 'This shoes is nice',
+    price: '100',
+    image:
+      'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+  },
+  {
+    id: 2,
+    name: 'Slippers',
+    description: 'This shoes is nice',
+    price: '100',
+    image:
+      'https://images.unsplash.com/photo-1460353581641-37baddab0fa2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80',
+  },
+  {
+    id: 3,
+    name: 'Sandals',
+    description: 'This shoes is nice',
+    price: '100',
+    image:
+      'https://images.unsplash.com/photo-1575537302964-96cd47c06b1b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+  },
+  {
+    id: 4,
+    name: 'Heels',
+    description: 'This shoes is nice',
+    price: '100',
+    image:
+      'https://images.unsplash.com/photo-1549298916-b41d501d3772?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1300&q=80',
+  },
+  {
+    id: 5,
+    name: 'Sneakers',
+    description: 'This shoes is nice',
+    price: '100',
+    image:
+      'https://images.unsplash.com/photo-1549298916-b41d501d3772?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1300&q=80',
+  },
+  {
+    id: 6,
+    name: 'Climbing Shoes',
+    description: 'This shoes is nice',
+    price: '100',
+    image:
+      'https://images.unsplash.com/photo-1551107696-a4b0c5a0d9a2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1300&q=80',
+  },
+  {
+    id: 7,
+    name: 'Sports Shoes',
+    description: 'This shoes is nice',
+    price: '100',
+    image:
+      'https://images.unsplash.com/photo-1551107696-a4b0c5a0d9a2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1300&q=80',
+  },
+]
